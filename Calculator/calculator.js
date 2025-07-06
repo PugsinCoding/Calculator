847307c23c823ac2d94f5584344b61276bb9717b
Chase Audrey S. <101994395+PugsinCoding@users.noreply.github.com>
2025-07-05 20:34:52 -0700

    Add files via upload
    Allows input to be shown on screen
    Allows to clear screen when pressed AC
    Allows for result to be seen on screen
    Still needs work on parentheses
    Still needs work on creating space so more equations can be used on the result

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -11,14 +11,29 @@ let nonNums = [
     ")"
 ]
 
+var equation = []
 
-function hello(idName) {
-    console.log(idName);
+function defState(){
+    offClicked();
+}
+
+function clickButton(buttonName){
+    let screenText = document.getElementById("screen-text");
+    screenText.innerHTML = "";
+    if(buttonName != "AC" && buttonName != "ON" && buttonName != "OFF" && buttonName != "="){
+        equation.push(buttonName);
+    }
+    let i = 0;
+    while(i < equation.length && i < 15){
+        screenText.innerHTML += equation[i];
+        i++;
+    }
 }
 
 function offClicked(){
     let screenText = document.getElementById("screen-text");
-    screenText.innerHTML = "OFF";
+    screenText.innerHTML = "";
+    equation = [];
     for(var i = 0; i <= 9; i++){
         let button = document.getElementById(i);
         button.style.pointerEvents = 'none';
@@ -33,10 +48,10 @@ function offClicked(){
     let button = document.getElementById("ON");
     button.style.pointerEvents = 'auto';
     button.style.backgroundColor = 'black';
-    
 }
 
 function onClicked(){
+    equation = [];
     let screenText = document.getElementById("screen-text");
     screenText.innerHTML = "00000000";
     for(var i = 0; i <= 9; i++){
@@ -53,4 +68,20 @@ function onClicked(){
     let button = document.getElementById("ON");
     button.style.pointerEvents = 'none';
     button.style.backgroundColor = 'gray';
+}
+
+function cleared(){
+    onClicked();
+}
+
+function goofingAround(){
+    let screenText = document.getElementById("screen-text");
+    let eqStr = "";
+    let i = 0;
+    while(i < equation.length && i < 15){
+        eqStr += equation[i];
+        i++;
+    }
+    let result = eval(eqStr);
+    screenText.innerHTML = result;
 }
